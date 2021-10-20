a449e1b9613ace873b35727573df9b897105aecd
Luiz Moura <luiz.oliveiramoura@hotmail.com>
2021-10-19 23:07:23 -0400

    chore: create signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import SignIn from './pages/signIn';
+// import SignIn from './pages/signIn';
+import SignUp from './pages/signUp';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );
